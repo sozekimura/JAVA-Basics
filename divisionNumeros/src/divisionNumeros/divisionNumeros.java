22ac667b059827e11b47cec947627156ba2ac0c9
Söze Kimura <134236524+sozekimura@users.noreply.github.com>
2023-06-22 00:53:58 -0600

    Division de Numeros
    Se agrega carpeta de proyecto division de numeros.

---
+++ divisionNumeros/src/divisionNumeros/divisionNumeros.java
@@ -0,0 +1,20 @@
+package divisionNumeros;
+
+public class divisionNumeros
+
+{
+	public static void main(String[] args) {
+        int dividendo = 17;
+        int divisor = 3;
+
+        int cociente = dividendo / divisor;
+        int resto = dividendo % divisor;
+        double respuestaReal = (double) dividendo / divisor;
+
+        System.out.println("Cociente: " + cociente);
+        System.out.println("Resto: " + resto);
+        System.out.println("Respuesta real: " + respuestaReal);
+    }
+}
+
+//No estoy muy seguro de que este bien, pero pues aqui vamos.
